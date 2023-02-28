37c8dbab5a97f14722a72addf4cc01d33d2cd5fe
Alec German <kimarimt@protonmail.com>
2023-02-28 09:13:53 -0500

    finished structure of script

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+from string import ascii_lowercase
+from time import sleep
+import os
+
+
+def clear(secs=1):
+    sleep(secs)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def main():
-    print('Hello, World!')
+    is_running = True
+
+    while is_running:
+        option = input(
+            'Enter \'encode\' to encrypt or \'decode\' to decrypt: ')
+        message = input('Enter your message: ').lower()
+        shift = int(input('Enter the shift number: '))
+        new_message = cipher(option, message, shift)
+        print(f'\nHere\'s the encoded result: {new_message}')
+        clear(2)
+
+        again = input('Enter \'yes\' to restart or \'no\' to quit: ').lower()
+        if again == 'no':
+            is_running = False
+        clear()
+
+
+def cipher(option, message, shift_number):
+    letters = list(ascii_lowercase)
+    shifted = letters[shift_number:] + letters[:shift_number]
+
+    if option == 'encode':
+        scheme = {letter: code for letter, code in zip(letters, shifted)}
+    else:
+        scheme = {letter: code for letter, code in zip(shifted, letters)}
+
+    return ''.join([scheme[letter] for letter in message])
 
 
 if __name__ == '__main__':
